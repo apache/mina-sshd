3b3cc7951545b473f253235488bdd61c77e5b69d
Guillaume Nodet <gnodet@apache.org>
2010-09-22 14:51:34 +0000

    [SSHD-92] Add missing ASL headers and move the implementations into a new package
    git-svn-id: https://svn.apache.org/repos/asf/mina/sshd/trunk@1000008 13f79535-47bb-0310-9956-ffa450edef68

--- sshd-core/src/main/java/org/apache/sshd/client/keyverifier/AcceptAllServerKeyVerifier.java
+++ sshd-core/src/main/java/org/apache/sshd/client/keyverifier/AcceptAllServerKeyVerifier.java
@@ -16,15 +16,30 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sshd.client;
+package org.apache.sshd.client.keyverifier;
 
 import java.net.SocketAddress;
 import java.security.PublicKey;
 
 import org.apache.sshd.ClientSession;
+import org.apache.sshd.client.ServerKeyVerifier;
+import org.apache.sshd.common.util.BufferUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public interface ServerKeyVerifier {
+/**
+ * A ServerKeyVerifier that accepts all server keys.
+ */
+public class AcceptAllServerKeyVerifier implements ServerKeyVerifier {
+	protected final Logger log = LoggerFactory.getLogger(getClass());
 
-    boolean verifyServerKey(ClientSession sshClientSession, SocketAddress remoteAddress, PublicKey serverKey);
+	public static final ServerKeyVerifier INSTANCE = new AcceptAllServerKeyVerifier();
 
+	private AcceptAllServerKeyVerifier() {
+	}
+
+	public boolean verifyServerKey(ClientSession sshClientSession, SocketAddress remoteAddress, PublicKey serverKey) {
+		log.trace("Accepting key for " + remoteAddress + " key=" + BufferUtils.printHex(serverKey.getEncoded()));
+		return true;
+	}
 }
