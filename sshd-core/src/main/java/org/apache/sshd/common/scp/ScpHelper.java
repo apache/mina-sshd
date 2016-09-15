6d0d896b2e58faf7c5fc85713a47e62a256ab043
Guillaume Nodet <gnodet@apache.org>
2016-09-15 11:31:56 +0200

    [SSHD-698] Remove octal integers warning

--- sshd-core/src/main/java/org/apache/sshd/common/scp/ScpHelper.java
+++ sshd-core/src/main/java/org/apache/sshd/common/scp/ScpHelper.java
@@ -62,6 +62,7 @@ import org.apache.sshd.common.util.logging.AbstractLoggingBean;
 /**
  * @author <a href="mailto:dev@mina.apache.org">Apache MINA SSHD Project</a>
  */
+@SuppressWarnings("OctalInteger")
 public class ScpHelper extends AbstractLoggingBean implements SessionHolder<Session> {
     /**
      * Command prefix used to identify SCP commands
--- sshd-core/src/main/java/org/apache/sshd/common/subsystem/sftp/SftpConstants.java
+++ sshd-core/src/main/java/org/apache/sshd/common/subsystem/sftp/SftpConstants.java
@@ -28,6 +28,7 @@ import org.apache.sshd.common.util.logging.LoggingUtils;
 /**
  * @author <a href="mailto:dev@mina.apache.org">Apache MINA SSHD Project</a>
  */
+@SuppressWarnings("OctalInteger")
 public final class SftpConstants {
     public static final String SFTP_SUBSYSTEM_NAME = "sftp";
 
