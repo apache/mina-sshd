2721d4df3fe2c5a6e582e48c2d7387fe2f970958
The-Yoda <yoda.thi.master@gmail.com>
2019-02-12 12:08:07 +0200

    [SSHD-894] Fix Race condition when doing async auth.

--- sshd-core/src/main/java/org/apache/sshd/server/session/ServerUserAuthService.java
+++ sshd-core/src/main/java/org/apache/sshd/server/session/ServerUserAuthService.java
@@ -359,11 +359,12 @@ public class ServerUserAuthService extends AbstractCloseable implements Service,
                 sendWelcomeBanner(session);
             }
 
-            Buffer response = session.createBuffer(SshConstants.SSH_MSG_USERAUTH_SUCCESS, Byte.SIZE);
-            session.writePacket(response);
             session.setUsername(username);
             session.setAuthenticated();
             session.startService(authService, buffer);
+
+            Buffer response = session.createBuffer(SshConstants.SSH_MSG_USERAUTH_SUCCESS, Byte.SIZE);
+            session.writePacket(response);
             session.resetIdleTimeout();
             log.info("Session {}@{} authenticated", username, session.getIoSession().getRemoteAddress());
         } else {
