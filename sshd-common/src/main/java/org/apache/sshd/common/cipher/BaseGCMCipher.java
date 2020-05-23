0b12782abbb030f03249d643457fd95cb0e36e1e
Matt Sicker <boards@gmail.com>
2020-05-22 20:55:18 -0500

    Fix iv reuse exception
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- sshd-common/src/main/java/org/apache/sshd/common/cipher/BaseGCMCipher.java
+++ sshd-common/src/main/java/org/apache/sshd/common/cipher/BaseGCMCipher.java
@@ -24,6 +24,8 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.apache.sshd.common.util.security.SecurityUtils;
+
 public class BaseGCMCipher extends BaseCipher {
 
     private Mode mode;
@@ -37,67 +39,20 @@ public class BaseGCMCipher extends BaseCipher {
     }
 
     @Override
-    protected byte[] initializeIVData(Mode mode, byte[] iv, int reqLen) {
-        parameters
-                = new CounterGCMParameterSpec(getAuthenticationTagSize() * Byte.SIZE, super.initializeIVData(mode, iv, reqLen));
-        parameters.update(-1);
-        return parameters.getIV();
-    }
-
-    @Override
-    protected byte[] initializeKeyData(Mode mode, byte[] key, int reqLen) {
-        byte[] keyData = super.initializeKeyData(mode, key, reqLen);
-        secretKey = new SecretKeySpec(keyData, getAlgorithm());
-        return keyData;
-    }
-
-    @Override
     protected Cipher createCipherInstance(Mode mode, byte[] key, byte[] iv) throws Exception {
         this.mode = mode;
-        Cipher cipher = super.createCipherInstance(mode, key, iv);
-        cipher.init(mode == Mode.Encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, secretKey, parameters);
-        return cipher;
+        secretKey = new SecretKeySpec(key, getAlgorithm());
+        parameters = new CounterGCMParameterSpec(getAuthenticationTagSize() * Byte.SIZE, iv);
+        return SecurityUtils.getCipher(getTransformation());
     }
 
     @Override
     public void updateWithAAD(byte[] input, int aadOffset, int aadLen, int inputOffset, int inputLen) throws Exception {
         Cipher cipher = getCipherInstance();
-        parameters.update(1);
+        parameters.incrementCounter();
         cipher.init(mode == Mode.Encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, secretKey, parameters);
         cipher.updateAAD(input, aadOffset, aadLen);
         cipher.doFinal(input, inputOffset, inputLen, input, inputOffset);
-    }
-
-    public static void encodeLong(long val, byte[] buf, int off) {
-        int dataLen = buf.length - off;
-        if (dataLen < Long.BYTES) {
-            throw new IndexOutOfBoundsException(
-                    "Available data length (" + dataLen + ") cannot accommodate 64-bit integer encoding");
-        }
-        buf[off] = (byte) (val >> 56);
-        buf[off + 1] = (byte) (val >> 48);
-        buf[off + 2] = (byte) (val >> 40);
-        buf[off + 3] = (byte) (val >> 32);
-        buf[off + 4] = (byte) (val >> 24);
-        buf[off + 5] = (byte) (val >> 16);
-        buf[off + 6] = (byte) (val >> 8);
-        buf[off + 7] = (byte) val;
-    }
-
-    public static long decodeLong(byte[] buf, int off, int len) {
-        if (len < Long.BYTES) {
-            throw new IndexOutOfBoundsException(
-                    "Available data length (" + len + ") cannot accommodate 64-bit integer encoding");
-        }
-        long l = ((long) buf[off] << 56) & 0xff00000000000000L;
-        l |= ((long) buf[off + 1] << 48) & 0x00ff000000000000L;
-        l |= ((long) buf[off + 2] << 40) & 0x0000ff0000000000L;
-        l |= ((long) buf[off + 3] << 32) & 0x000000ff00000000L;
-        l |= ((long) buf[off + 4] << 24) & 0x00000000ff000000L;
-        l |= ((long) buf[off + 5] << 16) & 0x0000000000ff0000L;
-        l |= ((long) buf[off + 6] << 8) & 0x000000000000ff00L;
-        l |= (buf[off + 7]) & 0x00000000000000ffL;
-        return l;
     }
 
     /**
@@ -108,18 +63,20 @@ public class BaseGCMCipher extends BaseCipher {
 
         protected CounterGCMParameterSpec(int tLen, byte[] src) {
             super(tLen, src);
-            iv = src.clone();
+            iv = src;
         }
 
-        protected void update(int delta) {
-            long counter = decodeLong(iv, Integer.BYTES, Long.BYTES);
-            counter = (counter + delta) & 0x0ffffffffL;
-            encodeLong(counter, iv, Integer.BYTES);
+        protected void incrementCounter() {
+            int i = iv.length - 1;
+            while (i >= iv.length - Long.BYTES && ++iv[i] == 0) {
+                // carry to next byte
+                i--;
+            }
         }
 
         @Override
         public byte[] getIV() {
-            return iv;
+            return iv.clone();
         }
     }
 
