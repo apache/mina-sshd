803fcb8aefd46f85c5fc9ba24136c6c50596b706
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2021-05-10 22:26:26 -0400

    SSHD-1161 - setting up unit test @Parameters to run all cert types

--- sshd-core/src/test/java/org/apache/sshd/client/opensshcerts/ClientOpenSSHCertificatesTest.java
+++ sshd-core/src/test/java/org/apache/sshd/client/opensshcerts/ClientOpenSSHCertificatesTest.java
@@ -11,6 +11,8 @@ import org.apache.sshd.common.util.io.IoUtils;
 import org.apache.sshd.util.test.CommonTestSupportUtils;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
@@ -22,9 +24,38 @@ import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+@RunWith(Parameterized.class) // see https://github.com/junit-team/junit/wiki/Parameterized-tests
 public class ClientOpenSSHCertificatesTest {
+
+  private static final String USER_KEY_PATH = "org/apache/sshd/client/opensshcerts/user/";
+
+  @Parameterized.Parameters(name = "key: {0}")
+  public static Iterable<? extends String> data() {
+    return Arrays.asList(
+      "user01_rsa_sha2_256_2048",
+      "user01_rsa_sha2_512_2048",
+      "user01_rsa_sha2_256_4096",
+      "user01_rsa_sha2_512_4096",
+      "user01_ed25519",
+      "user01_ecdsa_256",
+      "user01_ecdsa_384",
+      "user01_ecdsa_521"
+    );
+  }
+
+  @Parameterized.Parameter // first data value (0) is default
+  public String privateKeyName;
+
+  private String getPrivateKeyResource() {
+    return USER_KEY_PATH + privateKeyName;
+  }
+
+  private String getCertificateResource() {
+    return USER_KEY_PATH + privateKeyName + "-cert.pub";
+  }
 
   @Rule
   public GenericContainer sshdContainer = new GenericContainer(
@@ -45,12 +76,11 @@ public class ClientOpenSSHCertificatesTest {
     .withEnv("SSH_HOST_KEY", "/keys/host/host01")
     .withExposedPorts(22);
 
-
   @Test
-  public void sshClientCertificateRSATest() throws Exception {
+  public void clientCertAuth() throws Exception {
 
     try (final InputStream certInputStream =
-           Thread.currentThread().getContextClassLoader().getResourceAsStream("org/apache/sshd/client/opensshcerts/user/user01_rsa_sha2_512_2048-cert.pub")
+           Thread.currentThread().getContextClassLoader().getResourceAsStream(getCertificateResource())
     ) {
 
       final byte[] certBytes = IoUtils.toByteArray(certInputStream);
@@ -59,7 +89,7 @@ public class ClientOpenSSHCertificatesTest {
       final PublicKeyEntry certPublicKeyEntry = PublicKeyEntry.parsePublicKeyEntry(certLine);
       final PublicKey certPublicKey = certPublicKeyEntry.resolvePublicKey(null, null, null);
 
-      final FileKeyPairProvider keyPairProvider = CommonTestSupportUtils.createTestKeyPairProvider("org/apache/sshd/client/opensshcerts/user/user01_rsa_sha2_512_2048");
+      final FileKeyPairProvider keyPairProvider = CommonTestSupportUtils.createTestKeyPairProvider(getPrivateKeyResource());
 
       final KeyPair keypair = keyPairProvider.loadKeys(null).iterator().next();
 
