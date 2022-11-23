apache/mina-sshd

commit de2f8fef2e53263f5beef7dca1f3c048fcf59d2e

Asynchronous handling of global requests
Handling of global requests with want-reply=true was synchronous. This
is fine if the call is made on a non-I/O thread, but to be able to use
global requests in I/O threads, an asynchronous handling is needed.

Provide an asynchronous variant of Session.request() that returns a
future and that takes an optional reply handler as argument. If a
handler is given, it will be invoked by the framework once the reply
is received, on the I/O thread that received the reply. The future can
in this case be used to wait until the request has indeed be sent.
Without handler, the future can be used to await the reply on some
other thread, or to handle it via a future listener.

This makes global request handling much more flexible, avoids that a
synchronous implementation blocks an I/O thread, and it avoids problems
with making global requests while a key exchange is ongoing.

It also removes the restriction that there must be only one pending
global request per SSH session. The implementation keeps a FIFO list
of requests sent, and associates a reply with the front-most request
in that list. Only requests with want-reply=true enter that list.

Because of SSHD-968 we must also know the SSH message sequence number
of each global request made with want-reply=true. The previous
implementation assumed that the when a request was made, the next
packet written would be that request. But that may not be true when a
KEX is ongoing. Improve that implementation to report the sequence
number via a callback when the request's packet is indeed sent.

In AbstractSession.preClose(), fail all pending global requests.

Additionally, make Session.request() also work for global requests with
want-reply=false. Such requests can also be written directly with
Session.writePacket(), but it seems natural and convenient for users
of the library to also be able to use Session.request() for this.

sshd-core/src/main/java/org/apache/sshd/client/session/ClientConnectionService.java
@@ -20,6 +20,8 @@ package org.apache.sshd.client.session;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -28,6 +30,7 @@ import org.apache.sshd.agent.common.AgentForwardSupport;
 import org.apache.sshd.common.FactoryManager;
 import org.apache.sshd.common.SshConstants;
 import org.apache.sshd.common.SshException;
+import org.apache.sshd.common.future.GlobalRequestFuture;
 import org.apache.sshd.common.io.IoWriteFuture;
 import org.apache.sshd.common.session.Session;
 import org.apache.sshd.common.session.helpers.AbstractConnectionService;
@@ -125,11 +128,33 @@ public class ClientConnectionService
             buf.putBoolean(withReply);
 
             if (withReply) {
-                Buffer reply = session.request(heartbeatRequest, buf, heartbeatReplyMaxWait);
-                if (reply != null) {
+                Instant start = Instant.now();
+                CountDownLatch replyReceived = new CountDownLatch(1);
+                GlobalRequestFuture writeFuture = session.request(buf, heartbeatRequest, (cmd, reply) -> {
+                    replyReceived.countDown();
                     if (log.isTraceEnabled()) {
-                        log.trace("sendHeartBeat({}) received reply size={} for request={}",
-                                session, reply.available(), heartbeatRequest);
+                        log.trace("sendHeartBeat({}) received reply={} size={} for request={}", session,
+                                SshConstants.getCommandMessageName(cmd), reply.available(), heartbeatRequest);
+                    }
+                });
+                writeFuture.await(heartbeatReplyMaxWait);
+                Throwable t = writeFuture.getException();
+                if (t != null) {
+                    // We couldn't even send the request.
+                    throw new IOException(t.getMessage(), t);
+                }
+                Duration elapsed = Duration.between(start, Instant.now());
+                if (elapsed.compareTo(heartbeatReplyMaxWait) < 0) {
+                    long toWait = heartbeatReplyMaxWait.minus(elapsed).toMillis();
+                    if (toWait > 0) {
+                        try {
+                            replyReceived.await(toWait, TimeUnit.MILLISECONDS);
+                        } catch (InterruptedException e) {
+                            if (log.isTraceEnabled()) {
+                                log.trace("sendHeartBeat({}) interrupted waiting for reply to request={}", session,
+                                        heartbeatRequest);
+                            }
+                        }
                     }
                 }
             } else {

sshd-core/src/main/java/org/apache/sshd/common/future/GlobalRequestFuture.java
@@ -1,0 +1,175 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sshd.common.future;
+
+import org.apache.sshd.common.SshConstants;
+import org.apache.sshd.common.SshException;
+import org.apache.sshd.common.io.IoWriteFuture;
+import org.apache.sshd.common.util.GenericUtils;
+import org.apache.sshd.common.util.buffer.Buffer;
+
+/**
+ * A {@link DefaultSshFuture} that can be used to wait for the reply of an SSH_MSG_GLOBAL_REQUEST sent with
+ * {@code want-reply = true}.
+ *
+ * @see {@link org.apache.sshd.common.session.Session#request(Buffer, String, ReplyHandler)}
+ */
+public class GlobalRequestFuture extends DefaultSshFuture<GlobalRequestFuture>
+        implements SshFutureListener<IoWriteFuture> {
+
+    /**
+     * A {@code ReplyHandler} is invoked asynchronously when the reply for a request with {@code want-reply = true} is
+     * received. It is <em>not</em> invoked if the request could not be sent; to catch such cases
+     * {@link DefaultSshFuture#await()} the {@link GlobalRequestFuture} and check
+     * {@link GlobalRequestFuture#getException()}.
+     */
+    @FunctionalInterface
+    public interface ReplyHandler {
+
+        /**
+         * Invoked by the framework upon reception of the reply. If the global request was sent with
+         * {@code want-reply = false}, it is invoked with {@link SshConstants#SSH_MSG_REQUEST_SUCCESS} and an empty
+         * buffer after the request was successfully sent.
+         *
+         * @param cmd    the command received, can be one of {@link SshConstants#SSH_MSG_REQUEST_SUCCESS},
+         *               {@link SshConstants#SSH_MSG_UNIMPLEMENTED}, or {@link SshConstants#SSH_MSG_REQUEST_FAILURE}
+         * @param buffer the {@link Buffer} received
+         */
+        void accept(int cmd, Buffer buffer);
+    }
+
+    private final ReplyHandler handler;
+
+    private long sequenceNumber;
+
+    /**
+     * Creates a new {@link GlobalRequestFuture} for a global request. Synchronization occurs on the future itself. The
+     * future will be fulfilled once the reply has been received or an error occurred.
+     *
+     * @param request the request identifier
+     */
+    public GlobalRequestFuture(String request) {
+        this(request, null);
+    }
+
+    /**
+     * Creates a new {@link GlobalRequestFuture} for a global request. Synchronization occurs on the future itself. The
+     * future will be fulfilled once the request has been sent, or an error occurred during sending. The framework will
+     * invoke the handler once the reply has been received. For global requests with {@code want-reply = false}, the
+     * handler will be invoked with an empty buffer if the request was successfully sent.
+     *
+     * @param request the request identifier
+     * @param handler the {@link ReplyHandler}, or {@code null}
+     */
+    public GlobalRequestFuture(String request, ReplyHandler handler) {
+        super(request, null);
+        this.handler = handler;
+    }
+
+    @Override
+    public String getId() {
+        return (String) super.getId();
+    }
+
+    /**
+     * Retrieves this future's packet sequence number.
+     *
+     * @return the sequence number
+     */
+    public long getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    /**
+     * Sets the packet sequence number of the global request represented by this future.
+     *
+     * @param  number                   the packet sequence number
+     * @throws IllegalArgumentException if the number given is not an unsigned 32bit value
+     */
+    public void setSequenceNumber(long number) {
+        if (number < 0 || ((number & 0xFFFF_FFFFL) != number)) {
+            throw new IllegalArgumentException("Invalid sequence number " + number);
+        }
+        sequenceNumber = number;
+    }
+
+    /**
+     * Fulfills this future, marking it as failed.
+     *
+     * @param message An explanation of the failure reason
+     */
+    public void fail(String message) {
+        setValue(new SshException(GenericUtils.isEmpty(message) ? "Global request failure; unknown reason" : message));
+    }
+
+    /**
+     * Retrieves the {@link ReplyHandler} of this future, if any.
+     *
+     * @return the handler, or {@code null} if none was set
+     */
+    public ReplyHandler getHandler() {
+        return handler;
+    }
+
+    /**
+     * Obtains the reply {@link Buffer} if the request was successful. If called after {@link #isDone()} is
+     * {@code true}, a non-{@code null} result means the request was successful.
+     *
+     * @return the {@link Buffer}, or {@code null} if the request was not successful or the reply was not received yet
+     */
+    public Buffer getBuffer() {
+        Object value = getValue();
+        if (value instanceof Buffer) {
+            return (Buffer) value;
+        }
+        return null;
+    }
+
+    /**
+     * Retrieves an exception if the request failed. If called after {@link #isDone()} is {@code true}, a
+     * {@code null} result means the request did not fail.
+     *
+     * @return a failure reason, or {@code null} if there isn't one or if the request did not fail
+     */
+    public Throwable getException() {
+        Object value = getValue();
+        if (value instanceof Throwable) {
+            return (Throwable) value;
+        }
+        return null;
+    }
+
+    @Override
+    public void operationComplete(IoWriteFuture future) {
+        if (!future.isWritten()) {
+            // Sending the request message failed
+            Throwable ioe = future.getException();
+            if (ioe != null) {
+                setValue(ioe);
+            } else {
+                fail("Could not write global request " + getId() + " seqNo=" + getSequenceNumber());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "[seqNo=" + sequenceNumber + ']';
+    }
+}

commit 29ed50c855459cf9b4cacb610f0a6cb4aac53ccb

GH-268: Fix ClientConnectionService.sendHeartBeat()
If a reply is requested, but none arrives within the timeout, throw an
exception and terminate the connection.

This rolls back the changes made in ClientConnectionService in commit
de2f8fef. It's quite all right to use the synchronous implementation of
Session.request() because heartbeats are not sent from I/O threads.

However, commit de2f8fef broke the contract specified in interface
Session, which says Session.request() must return "the buffer if the
request was successful, {@code null} otherwise." The implementation
from commit de2f8fef threw an exception instead.

This was wrong, and is corrected now in this commit. Note that this
means that a caller cannot distinguish between the server replying
SSH_MSG_UNIMPLEMENTED or SSH_MSG_REQUEST_FAILURE.

Where such distinction is needed, use an asynchronous global request
instead.

Bug: https://github.com/apache/mina-sshd/issues/268

sshd-core/src/main/java/org/apache/sshd/client/session/ClientConnectionService.java
@@ -20,8 +20,6 @@ package org.apache.sshd.client.session;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.time.Instant;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -30,7 +28,6 @@ import org.apache.sshd.agent.common.AgentForwardSupport;
 import org.apache.sshd.common.FactoryManager;
 import org.apache.sshd.common.SshConstants;
 import org.apache.sshd.common.SshException;
-import org.apache.sshd.common.future.GlobalRequestFuture;
 import org.apache.sshd.common.io.IoWriteFuture;
 import org.apache.sshd.common.session.Session;
 import org.apache.sshd.common.session.helpers.AbstractConnectionService;
@@ -128,33 +125,11 @@ public class ClientConnectionService
             buf.putBoolean(withReply);
 
             if (withReply) {
-                Instant start = Instant.now();
-                CountDownLatch replyReceived = new CountDownLatch(1);
-                GlobalRequestFuture writeFuture = session.request(buf, heartbeatRequest, (cmd, reply) -> {
-                    replyReceived.countDown();
+                Buffer reply = session.request(heartbeatRequest, buf, heartbeatReplyMaxWait);
+                if (reply != null) {
                     if (log.isTraceEnabled()) {
-                        log.trace("sendHeartBeat({}) received reply={} size={} for request={}", session,
-                                SshConstants.getCommandMessageName(cmd), reply.available(), heartbeatRequest);
-                    }
-                });
-                writeFuture.await(heartbeatReplyMaxWait);
-                Throwable t = writeFuture.getException();
-                if (t != null) {
-                    // We couldn't even send the request.
-                    throw new IOException(t.getMessage(), t);
-                }
-                Duration elapsed = Duration.between(start, Instant.now());
-                if (elapsed.compareTo(heartbeatReplyMaxWait) < 0) {
-                    long toWait = heartbeatReplyMaxWait.minus(elapsed).toMillis();
-                    if (toWait > 0) {
-                        try {
-                            replyReceived.await(toWait, TimeUnit.MILLISECONDS);
-                        } catch (InterruptedException e) {
-                            if (log.isTraceEnabled()) {
-                                log.trace("sendHeartBeat({}) interrupted waiting for reply to request={}", session,
-                                        heartbeatRequest);
-                            }
-                        }
+                        log.trace("sendHeartBeat({}) received reply size={} for request={}",
+                                session, reply.available(), heartbeatRequest);
                     }
                 }
             } else {

sshd-core/src/main/java/org/apache/sshd/common/future/GlobalRequestFuture.java
@@ -21,7 +21,6 @@ package org.apache.sshd.common.future;
 import org.apache.sshd.common.SshConstants;
 import org.apache.sshd.common.SshException;
 import org.apache.sshd.common.io.IoWriteFuture;
-import org.apache.sshd.common.util.GenericUtils;
 import org.apache.sshd.common.util.buffer.Buffer;
 
 /**
@@ -110,15 +109,6 @@ public class GlobalRequestFuture extends DefaultSshFuture<GlobalRequestFuture>
     }
 
     /**
-     * Fulfills this future, marking it as failed.
-     *
-     * @param message An explanation of the failure reason
-     */
-    public void fail(String message) {
-        setValue(new SshException(GenericUtils.isEmpty(message) ? "Global request failure; unknown reason" : message));
-    }
-
-    /**
      * Retrieves the {@link ReplyHandler} of this future, if any.
      *
      * @return the handler, or {@code null} if none was set
@@ -163,7 +153,7 @@ public class GlobalRequestFuture extends DefaultSshFuture<GlobalRequestFuture>
             if (ioe != null) {
                 setValue(ioe);
             } else {
-                fail("Could not write global request " + getId() + " seqNo=" + getSequenceNumber());
+                setValue(new SshException("Could not write global request " + getId() + " seqNo=" + getSequenceNumber()));
             }
         }
     }

sshd-core/src/main/java/org/apache/sshd/common/global/GlobalRequestException.java
@@ -1,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sshd.common.global;
+
+import org.apache.sshd.common.SshConstants;
+
+/**
+ * An exception that can be set on a {@link org.apache.sshd.common.future.GlobalRequestFuture} to indicate that the
+ * server sent back a failure reply.
+ */
+public class GlobalRequestException extends Exception {
+
+    private static final long serialVersionUID = 225802262556424684L;
+
+    private final int code;
+
+    /**
+     * Creates a new {@link GlobalRequestException} with the given SSH message code.
+     *
+     * @param code SSH message code to set; normally {@link SshConstants#SSH_MSG_UNIMPLEMENTED} or
+     *             {@link SshConstants#SSH_MSG_REQUEST_FAILURE}
+     */
+    public GlobalRequestException(int code) {
+        super(SshConstants.getCommandMessageName(code));
+        this.code = code;
+    }
+
+    /**
+     * Retrieves the SSH message code.
+     *
+     * @return the code, normally {@link SshConstants#SSH_MSG_UNIMPLEMENTED} or
+     *         {@link SshConstants#SSH_MSG_REQUEST_FAILURE}
+     */
+    public int getCode() {
+        return code;
+    }
+}

sshd-core/src/main/java/org/apache/sshd/common/session/helpers/AbstractSession.java
@@ -66,6 +66,7 @@ import org.apache.sshd.common.future.DefaultKeyExchangeFuture;
 import org.apache.sshd.common.future.DefaultSshFuture;
 import org.apache.sshd.common.future.GlobalRequestFuture;
 import org.apache.sshd.common.future.KeyExchangeFuture;
+import org.apache.sshd.common.global.GlobalRequestException;
 import org.apache.sshd.common.io.IoSession;
 import org.apache.sshd.common.io.IoWriteFuture;
 import org.apache.sshd.common.kex.KexProposalOption;
@@ -972,7 +973,7 @@ public abstract class AbstractSession extends SessionHelper {
             if (debugEnabled) {
                 log.debug("preClose({}): Session closing; failing still pending global request {}", this, future.getId());
             }
-            future.fail("Session is closing");
+            future.setValue(new SshException("Session is closing"));
         }
 
         // Fire 'close' event
@@ -1179,6 +1180,15 @@ public abstract class AbstractSession extends SessionHelper {
             if (!done || result == null) {
                 throw new SocketTimeoutException("No response received after " + maxWaitMillis + "ms for request=" + request);
             }
+            // The operation is specified to return null if the request could be made, but got an error reply.
+            // The caller cannot distinguish between SSH_MSG_UNIMPLEMENTED and SSH_MSG_REQUEST_FAILURE.
+            if (result instanceof GlobalRequestException) {
+                if (debugEnabled) {
+                    log.debug("request({}) request={}, requestSeqNo={}: received={}", this, request, future.getSequenceNumber(),
+                            SshConstants.getCommandMessageName(((GlobalRequestException) result).getCode()));
+                }
+                return null;
+            }
         }
 
         if (result instanceof Throwable) {
@@ -1290,7 +1300,7 @@ public abstract class AbstractSession extends SessionHelper {
                     Buffer resultBuf = ByteArrayBuffer.getCompactClone(buffer.array(), buffer.rpos(), buffer.available());
                     handler.accept(cmd, resultBuf);
                 } else {
-                    future.fail(SshConstants.getCommandMessageName(cmd));
+                    future.setValue(new GlobalRequestException(cmd));
                 }
                 return true; // message handled internally
             } else if (future != null) {
@@ -2204,7 +2214,7 @@ public abstract class AbstractSession extends SessionHelper {
                 Buffer resultBuf = ByteArrayBuffer.getCompactClone(buffer.array(), buffer.rpos(), buffer.available());
                 handler.accept(SshConstants.SSH_MSG_REQUEST_FAILURE, resultBuf);
             } else {
-                request.fail(SshConstants.getCommandMessageName(SshConstants.SSH_MSG_REQUEST_FAILURE));
+                request.setValue(new GlobalRequestException(SshConstants.SSH_MSG_REQUEST_FAILURE));
             }
         }
     }

sshd-core/src/test/java/org/apache/sshd/KeepAliveTest.java
@@ -217,6 +217,37 @@ public class KeepAliveTest extends BaseTestSupport {
         }
     }
 
+    @Test // see GH-268
+    public void testTimeoutOnMissingHeartbeatResponse() throws Exception {
+        CoreModuleProperties.IDLE_TIMEOUT.set(sshd, Duration.ofSeconds(30));
+        List<RequestHandler<ConnectionService>> globalHandlers = sshd.getGlobalRequestHandlers();
+        sshd.setGlobalRequestHandlers(Collections.singletonList(new AbstractConnectionServiceRequestHandler() {
+            @Override
+            public Result process(ConnectionService connectionService, String request, boolean wantReply, Buffer buffer)
+                    throws Exception {
+                // Never reply;
+                return Result.Replied;
+            }
+        }));
+        CoreModuleProperties.HEARTBEAT_INTERVAL.set(client, HEARTBEAT);
+        CoreModuleProperties.HEARTBEAT_REPLY_WAIT.set(client, Duration.ofSeconds(1));
+        try (ClientSession session = client.connect(getCurrentTestName(), TEST_LOCALHOST, port).verify(CONNECT_TIMEOUT)
+                .getSession()) {
+            session.addPasswordIdentity(getCurrentTestName());
+            session.auth().verify(AUTH_TIMEOUT);
+
+            try (ClientChannel channel = session.createChannel(Channel.CHANNEL_SHELL)) {
+                long waitStart = System.currentTimeMillis();
+                Collection<ClientChannelEvent> result = channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED), TIMEOUT);
+                long waitEnd = System.currentTimeMillis();
+                assertTrue("Wrong channel state after wait of " + (waitEnd - waitStart) + " ms: " + result,
+                        result.contains(ClientChannelEvent.CLOSED));
+            }
+        } finally {
+            sshd.setGlobalRequestHandlers(globalHandlers); // restore original
+        }
+    }
+
     public static class TestEchoShellFactory extends EchoShellFactory {
         public TestEchoShellFactory() {
             super();

sshd-core/src/test/java/org/apache/sshd/common/session/GlobalRequestTest.java
@@ -35,6 +35,7 @@ import org.apache.sshd.common.SshConstants;
 import org.apache.sshd.common.channel.RequestHandler;
 import org.apache.sshd.common.config.keys.KeyUtils;
 import org.apache.sshd.common.future.GlobalRequestFuture;
+import org.apache.sshd.common.global.GlobalRequestException;
 import org.apache.sshd.common.session.helpers.AbstractConnectionServiceRequestHandler;
 import org.apache.sshd.common.signature.Signature;
 import org.apache.sshd.common.util.GenericUtils;
@@ -197,7 +198,7 @@ public class GlobalRequestTest extends BaseTestSupport {
                     case 0: {
                         j++;
                         Buffer reply = request.getBuffer();
-                        assertTrue("Expected success for request " + i, reply != null);
+                        assertNotNull("Expected success for request " + i, reply);
                         assertEquals("Expected a success", (byte) ('1' + j - 1), reply.getByte());
                         break;
                     }
@@ -205,14 +206,20 @@ public class GlobalRequestTest extends BaseTestSupport {
                         j++;
                         failure = request.getException();
                         assertNotNull("Expected failure for request " + i, failure);
-                        assertEquals("Unexpected failure reason for request " + i, "SSH_MSG_REQUEST_FAILURE",
-                                failure.getMessage());
+                        assertTrue("Unexpected failure type", failure instanceof GlobalRequestException);
+                        assertEquals("Unexpected failure reason for request " + i, SshConstants.SSH_MSG_REQUEST_FAILURE,
+                                ((GlobalRequestException) failure).getCode());
+                        assertTrue("Unexpected failure message for request " + i,
+                                failure.getMessage().contains("SSH_MSG_REQUEST_FAILURE"));
                         break;
                     default:
                         failure = request.getException();
                         assertNotNull("Expected failure for request " + i, failure);
-                        assertEquals("Unexpected failure reason for request " + i, "SSH_MSG_UNIMPLEMENTED",
-                                failure.getMessage());
+                        assertTrue("Unexpected failure type", failure instanceof GlobalRequestException);
+                        assertEquals("Unexpected failure reason for request " + i, SshConstants.SSH_MSG_UNIMPLEMENTED,
+                                ((GlobalRequestException) failure).getCode());
+                        assertTrue("Unexpected failure message for request " + i,
+                                failure.getMessage().contains("SSH_MSG_UNIMPLEMENTED"));
                         break;
                 }
             }