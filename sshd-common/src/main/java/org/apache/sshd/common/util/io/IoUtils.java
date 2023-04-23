710e71f511ff5c5ab270947e52b1f4c236312e35
Andrew Pikler <andrew.pikler@earnix.com>
2023-04-23 19:52:19 +0300

    more github code review fix

--- sshd-common/src/main/java/org/apache/sshd/common/util/io/IoUtils.java
+++ sshd-common/src/main/java/org/apache/sshd/common/util/io/IoUtils.java
@@ -747,7 +747,8 @@ public final class IoUtils {
      * @param  namesList the parts of the path to build
      * @return           the built path
      */
-    public static Path buildPath(Path root, FileSystem fs, List<String> namesList) {
+    public static Path buildPath(Path root, FileSystem fs, Collection<String> namesList) {
+        Objects.requireNonNull(fs);
         if (namesList == null) {
             return null;
         }
@@ -767,7 +768,7 @@ public final class IoUtils {
      * @param  namesList the names list
      * @return           the built path
      */
-    public static Path buildRelativePath(FileSystem fs, List<String> namesList) {
+    public static Path buildRelativePath(FileSystem fs, Collection<String> namesList) {
         String[] names = new String[namesList.size() - 1];
 
         Iterator<String> it = namesList.iterator();
