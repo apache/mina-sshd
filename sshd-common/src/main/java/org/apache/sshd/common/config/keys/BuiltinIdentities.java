5c198ae90122f58f3f8bd65e8a7c015f14e3928d
Thomas Wolf <twolf@apache.org>
2025-05-10 22:27:50 +0200

    Fix formatting in BuiltinIdentities

--- sshd-common/src/main/java/org/apache/sshd/common/config/keys/BuiltinIdentities.java
+++ sshd-common/src/main/java/org/apache/sshd/common/config/keys/BuiltinIdentities.java
@@ -52,7 +52,7 @@ public enum BuiltinIdentities implements Identity {
     RSA(Constants.RSA, RSAPublicKey.class, RSAPrivateKey.class, KeyPairProvider.SSH_RSA),
     DSA(Constants.DSA, DSAPublicKey.class, DSAPrivateKey.class, KeyPairProvider.SSH_DSS),
     ECDSA(Constants.ECDSA, KeyUtils.EC_ALGORITHM, ECPublicKey.class, ECPrivateKey.class,
-            ECCurves.VALUES.stream().map(KeyTypeIndicator::getKeyType).collect(Collectors.toList())),
+          ECCurves.VALUES.stream().map(KeyTypeIndicator::getKeyType).collect(Collectors.toList())),
     ED25119(Constants.ED25519, SecurityUtils.EDDSA,
             SecurityUtils.getEDDSAPublicKeyType(),
             SecurityUtils.getEDDSAPrivateKeyType(),
